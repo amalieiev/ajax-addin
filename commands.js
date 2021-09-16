adf73b71c3492c5f665699a110957550a547ec16
artem.malieiev <artem.malieiev@exclaimer.com>
2021-09-16 17:28:25 +0300

    updatd commands with examples

--- commands.js
+++ commands.js
@@ -31,15 +31,58 @@ async function onMessageComposeHandler(event) {
 
     try {
         try {
-            debugMessage("Ajax Start");
+            debugMessage("Native fetch Start");
+            await fetch(
+                "https://amalieievfunctions.azurewebsites.net/api/get-signatures",
+                {
+                    mode: "cors",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer qwe123",
+                    },
+                    body: JSON.stringify({
+                        sender: "artem.malieiev@gmail.com",
+                    }),
+                }
+            );
+            debugMessage("Native fetch Success");
+        } catch (error) {
+            debugMessage("Native fetch Error");
+            debugMessage(error.message ? error.message : error);
+        }
+
+        try {
+            debugMessage("jQuery.ajax Start");
             await $.ajax({
                 url: "https://amalieievfunctions.azurewebsites.net/api/get-signatures",
                 dataType: "json",
                 headers: { Authorization: "Bearer qwe123" },
             });
-            debugMessage("Ajax Success");
+            debugMessage("jQuery.ajax Success");
         } catch (error) {
-            debugMessage("Ajax Error");
+            debugMessage("jQuery.ajax Error");
+            debugMessage(error.message ? error.message : error);
+        }
+
+        try {
+            debugMessage("Text/Plain Start");
+            await fetch(
+                "https://amalieievfunctions.azurewebsites.net/api/get-signatures",
+                {
+                    mode: "cors",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "text/plain",
+                    },
+                    body: JSON.stringify({
+                        sender: "artem.malieiev@gmail.com",
+                    }),
+                }
+            );
+            debugMessage("Text/Plain Success");
+        } catch (error) {
+            debugMessage("Text/Plain Error");
             debugMessage(error.message ? error.message : error);
         }
     } catch (error) {
